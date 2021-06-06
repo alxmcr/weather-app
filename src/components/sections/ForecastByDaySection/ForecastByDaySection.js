0f5420d006c66b0775c8db37dd7ec7758781d6b9
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-06 15:08:06 -0400

    Create Section: ForecastByDaySection

---
+++ src/components/sections/ForecastByDaySection/ForecastByDaySection.js
@@ -0,0 +1,31 @@
+import HourWeather from 'components/cards/HourWeather/HourWeather'
+import { useForecastByDay } from 'hooks/useForecastByDay'
+import styles from './ForecastByDaySection.module.css'
+
+const ForecastByDaySection = ({ nameCity = '' }) => {
+    const [forecastByDay, loading, error] = useForecastByDay(nameCity)
+
+    if (loading) return <p>Loading the weather for next hours...</p>
+    if (error) return <p>I'm so sorry, there was an error. Please, try again.</p>
+
+    return (
+        <section className={styles.ForecastByDaySection}>
+            <h2>Next Hours</h2>
+            {
+
+                forecastByDay.hour 
+                ? (
+                    <div className={styles.ForecastByDayHours}>
+                        {
+                            forecastByDay.hour.map((hourWeather, index) => (
+                                <HourWeather key={index} weather={hourWeather} />
+                            ))
+                        }
+                    </div>
+                ) : <p>There isn't any information about next hours.</p>
+            }
+        </section>
+    )
+}
+
+export default ForecastByDaySection;
