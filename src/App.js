9c97812499faf6a96cfc2e6dfc2e79d6926af161
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-06 15:10:23 -0400

    Working with the App and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import { SwitchRoutesApp } from 'routing/SwitchRoutesApp';
-import styles from './App.module.css';
+import styles from './App.module.css'
 
 function App() {
   return (
     <BrowserRouter>
       <div className={styles.App}>
         <SwitchRoutesApp />
+        Powered by <a href="https://www.weatherapi.com/" title="Free Weather API">WeatherAPI.com</a>
       </div>
     </BrowserRouter>
   );
